92b058df168b05780ce2c997d5256a99c7fbb407
Luna R <snowluna@protonmail.com>
2022-07-13 22:48:10 +0000

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Coding Bootcamp Module 6 Challenge
+// Weather Dashboard, Luna Roberge
+
+const main = function()
+{
+   // text input for search
+   searchEl = document.getElementById("search");
+}
+
+main();
+
---
+++ assets/priv/key.js
@@ -0,0 +1,3 @@
+const key = {
+      onecall = "c7ff47cc8579dbfa0a5297d6f83744c4"
+}
