14aae9975bfce60eeb6dce3eb8e9d1ffc530bb2f
Luna R <snowluna@protonmail.com>
2022-07-14 20:06:10 +0000

    add fetch functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,81 @@
 
 const main = function()
 {
-   // text input for search
+   // Form for search
    var searchEl = document.getElementById("search");
+   var searchInputEl = document.querySelector("#search input");
 
-   // fetch weather
    const baseRequestUrl = "https://api.openweathermap.org/data/3.0/onecall" + 
-      "?lat={lat}&lon={lon}&appid={key}";
+      `?lat={lat}&lon={lon}&appid=${key.api}`;
+   const baseGeoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct" + 
+      `?q={q}&limit=1&appid=${key.api}`;
 
+   // Fetch latitude and longitude from OpenWeatherMap Geocoding API
+   async function fetchCoordinates(location) 
+   {
+      if (!location) 
+      {
+         console.error("No location provided.");
+      }
+
+      else
+      {
+         requestUrl = baseGeoCodeUrl.replace("{q}", location);
+
+         try 
+         {
+            const response = await fetch(requestUrl,
+               {
+                  method: "GET",
+                  mode: "cors",
+                  credentials: "same-origin"
+               }
+            );
+
+            const locationData = await response.json();
+            return locationData;
+         }
+         
+         catch (err)
+         {
+            console.error(err);
+         }
+      }
+   }
+
+   // Fetch weather from OpenWeatherMap One Call API
+   async function fetchWeather(lat, lon)
+   {
+      if (!lat || !lon)
+         return false
+      else
+      {
+         try {
+            const response = await fetch(requestUrl,
+               {
+                  method: "GET",
+                  mode: "cors",
+                  credentials: "same-origin"
+               }
+            );
+
+            const weatherData = await response.json();
+            return weatherData;
+         }
+         catch (err)
+         {
+            console.error(err);
+         }
+      }
+   }
+
+   searchEl.addEventListener("submit", 
+      (e)=>
+      {
+         e.preventDefault();
+         console.log(searchInputEl.value.split(","))
+      }
+   )
 
 }
 
--- assets/priv/key.js
+++ assets/priv/key.js
@@ -1,3 +1,3 @@
 const key = {
-      onecall = "c7ff47cc8579dbfa0a5297d6f83744c4"
+   api: "c7ff47cc8579dbfa0a5297d6f83744c4"
 }
