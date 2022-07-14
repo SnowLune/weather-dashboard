d45ab37d3d4854b802c6099aade37ff38f2c24cb
Luna R <snowluna@protonmail.com>
2022-07-14 19:23:40 +0000

    add header, start js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,13 @@
 const main = function()
 {
    // text input for search
-   searchEl = document.getElementById("search");
+   var searchEl = document.getElementById("search");
+
+   // fetch weather
+   const baseRequestUrl = "https://api.openweathermap.org/data/3.0/onecall" + 
+      "?lat={lat}&lon={lon}&appid={key}";
+
+
 }
 
 main();
